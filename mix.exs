9753ba9d2eef70caa32f86482fd077a0f85f0e6c
Anton Zhiliuk <crashtown.pal@gmail.com>
2016-10-06 21:07:05 +0300

    bump version

--- mix.exs
+++ mix.exs
@@ -36,8 +36,8 @@ defmodule ExMachina.Mixfile do
     [
       {:ex_doc, "~> 0.9", only: :dev},
       {:earmark, ">= 0.0.0", only: :dev},
-      {:ecto, "~> 2.0", optional: true},
-      {:ecto, "~> 2.0", only: [:dev, :test]},
+      {:ecto, "~> 2.1-rc", optional: true},
+      {:ecto, "~> 2.1-rc", only: [:dev, :test]},
       {:postgrex, ">= 0.0.0", only: [:test]},
       {:poison, "~> 2.0.0", only: :test}
     ]
